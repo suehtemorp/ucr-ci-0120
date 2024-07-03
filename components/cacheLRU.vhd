6e38dd291a848a8a29e625d00e303a5d7799ccbc
Lesan2807 <lesan2807@gmail.com>
2024-07-02 20:18:43 -0600

    VHDL udpate

--- components/cacheLRU.vhd
+++ components/cacheLRU.vhd
@@ -23,8 +23,8 @@ ENTITY cacheLRU IS
   -- Output ports
 	hit : OUT std_logic; 
 	data_out: OUT std_logic_vector(31 DOWNTO 0);
-		tag_out : OUT std_logic_vector (16 DOWNTO 0); 
-		index_out : OUT std_logic_vector (8 DOWNTO 0); 
+		tag_out : OUT std_logic_vector (20 DOWNTO 0); 
+		index_out : OUT std_logic_vector (4 DOWNTO 0); 
 		offset_out :OUT std_logic_vector (5 DOWNTO 0);
 	miss_out: OUT std_logic;
 	set_0: OUT std_logic; 
@@ -38,8 +38,8 @@ END cacheLRU;
 
 ARCHITECTURE TypeArchitecture OF cacheLRU IS
 
--- Cache block definition: array of 64 blocks of 17-bit words
-type bloque is array(0 to 63) of std_logic_vector(16 downto 0);
+-- Cache block definition: array of 64 blocks of 20-bit words
+type bloque is array(0 to 63) of std_logic_vector(20 downto 0);
 
 -- Cache set definition: array of 256 cache blocks
 type t_set is array(0 to 255) of bloque;
@@ -49,14 +49,14 @@ signal cache0 : t_set;
 signal cache1 : t_set; 
 
 -- Tag, valid, dirty, and LRU signal definitions
-type t_tag is array(0 to 255) of std_logic_vector(16 downto 0);
+type t_tag is array(0 to 255) of std_logic_vector(20 downto 0);
 type t_valid is array(0 to 255) of std_logic;
 type t_Dirty is array(0 to 255) of std_logic;
 type t_LRU is array(0 to 255) of std_logic; 
 
 -- Internal signals for tag, index, offset, and data
-signal s_tag : std_logic_vector(16 downto 0);
-signal s_index :std_logic_vector (8 DOWNTO 0); 
+signal s_tag : std_logic_vector(20 downto 0);
+signal s_index :std_logic_vector (4 DOWNTO 0); 
 signal s_offset : std_logic_vector (5 DOWNTO 0); 
 signal data: std_logic_vector(31 DOWNTO 0); 
 signal hit_addr : std_logic;
@@ -68,12 +68,12 @@ signal mainmem : t_Memory:= ("00000000000000000000000000000001",others =>"000000
 -- Signals for dirty, valid, tag, and LRU bits for both cache sets
 signal dirtys0 : t_Dirty:= ( others => '0');
 signal valids0 : t_valid:= ( others => '0');
-signal tags0 : t_tag:=( others => "00000000000000000");
+signal tags0 : t_tag:=( others => "000000000000000000000");
 signal lru0: t_LRU:=(others => '0'); 
 
 signal dirtys1 : t_Dirty:= ( others => '0');
 signal valids1 : t_valid:= (others => '0');
-signal tags1 : t_tag:=( others => "00000000000000000");
+signal tags1 : t_tag:=( others => "000000000000000000000");
 signal lru1: t_LRU:=(others => '1'); 
 
 -- Signal for miss logic
@@ -82,18 +82,21 @@ signal miss : std_logic;
 signal set_0_out: std_logic;
 signal set_1_out : std_logic; 
 
+
+
 BEGIN
 -- Process that handles the cache behavior on clock edge
 process (clock)
 	begin
 	if clock = '1' then
 	-- Extracting tag, index, and offset from the address when clock is in its positive state 
-		s_tag <= Address(31 DOWNTO 15); 
-		s_index <= Address(14  DOWNTO 6); 
+		s_tag <= Address(31 DOWNTO 11); 
+		s_index <= Address(10  DOWNTO 6); 
 		s_offset <= Address(5  DOWNTO 0); 
 		-- Default values for hit and miss signals 
 		hit_addr <= '0'; 
 		miss <= '1'; 
+
 		-- Check if the address is in the cache (this is when valid bit and tag match) 
 		if valids0(to_integer(unsigned(s_index))) = '1' or valids1(to_integer(unsigned(s_index))) = '1' then 
 			if s_tag = tags0(to_integer(unsigned(s_index))) or s_tag = tags1(to_integer(unsigned(s_index))) then 
@@ -102,10 +105,10 @@ process (clock)
 				hit_addr <= '1'; 
 				miss <= '0';
 			end if; 
-		end if;
+		end if; 
+		-- Determine which set to used based on LRU bit. Check if the tag is not stored already in the cache 
+		if lru0(to_integer(unsigned(s_index))) = '0'and tags1(to_integer(unsigned(s_index))) /= s_tag  then 
 
-		-- Determine which set to used based on LRU bit 
-		if lru0(to_integer(unsigned(s_index))) = '0' then
 				-- Use set 0 
 				valids0(to_integer(unsigned(s_index))) <= '1';
 				
@@ -119,11 +122,10 @@ process (clock)
 				-- Add the tag to tags0 
 				tags0(to_integer(unsigned(s_index))) <= s_tag;
 
-				if hit_addr = '0' then 
-					set_1_out <= '0'; 
-					set_0_out <= '1';
-				end if;  
-		else
+		
+				set_1_out <= '0'; 
+				set_0_out <= '1';
+		elsif lru1(to_integer(unsigned(s_index))) = '0' and tags0(to_integer(unsigned(s_index))) /= s_tag then 
 				-- Use set 1 
 				valids1(to_integer(unsigned(s_index))) <= '1';
 				
@@ -135,12 +137,11 @@ process (clock)
 
 				-- Add the tag to tags1
 				tags1(to_integer(unsigned(s_index))) <= s_tag;
-				
-				if hit_addr = '0' then
-					set_1_out <= '1'; 
-					set_0_out <= '0';
-				end if;  
-		end if; 
+
+
+				set_1_out <= '1'; 
+				set_0_out <= '0'; 
+		end if;
 	end if; 
 end process;
 
