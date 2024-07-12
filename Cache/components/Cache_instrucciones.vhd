c255e8282ea698335cdd3cf5edc0e2249628638b
Lucia <lesan2807@gmail.com>
2024-07-12 15:24:01 -0600

    Cache de instrucciones

--- Cache/components/Cache_instrucciones.vhd
+++ Cache/components/Cache_instrucciones.vhd
@@ -8,7 +8,7 @@
 -- Description :
 --
  --128KB 
--- Address are size 24
+-- Address are size 30
 --------------------------------------------------------------------------------
 
 LIBRARY ieee;
@@ -22,7 +22,7 @@ ENTITY Cache_instrucciones IS
   ------------------------------------------------------------------------------
   --Insert input ports below
  	clock      : IN  std_logic;           
-	Address: IN std_logic_vector(23 DOWNTO 0);
+	Address: IN std_logic_vector(29 DOWNTO 0);
   	Data_From_Mem: IN std_logic_vector(31 DOWNTO 0); 
   ------------------------------------------------------------------------------
 	hit : OUT std_logic; 
@@ -39,13 +39,13 @@ type t_bloque is array(0 to 63) of std_logic_vector(31 downto 0);
 type t_set is array(0 to 2048) of t_bloque;
 
 signal cache0 : t_set;
-type t_tag is array(0 to 2048) of std_logic_vector(6 downto 0);
+type t_tag is array(0 to 2048) of std_logic_vector(12 downto 0);
 type t_valid is array(0 to 2048) of std_logic;
 
 signal valids0 : t_valid:= ( others => '0');
-signal tags0 : t_tag:=( others => "0000000");
+signal tags0 : t_tag:=( others => "0000000000000");
 
-signal s_tag : std_logic_vector(23 DOWNTO 17);
+signal s_tag : std_logic_vector(29 DOWNTO 17);
 signal s_index :std_logic_vector (16 DOWNTO 6); 
 signal s_offset : std_logic_vector (5 DOWNTO 0); 
 
@@ -61,7 +61,7 @@ process (clock)
 	begin
 	if clock = '1' then
 		-- Extracting tag, index, and offset from the address when clock is in its positive state 
-		s_tag    <= Address(23 DOWNTO 17); 
+		s_tag    <= Address(29 DOWNTO 17); 
 		s_index  <= Address(16 DOWNTO 6); 
 		s_offset <= Address(5 DOWNTO 0); 
 		-- Default values for hit and miss signals 
